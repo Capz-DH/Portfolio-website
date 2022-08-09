0e378241ac0309b2dd850a1df26fb398ae5b920a
Devon-Henry <108783601+Capz-DH@users.noreply.github.com>
2022-08-09 17:41:14 +0100

    Add files via upload
    fixed bug of immediately appearing and disappearing side bar and other QOL improvements

--- script.js
+++ script.js
@@ -1,43 +1,53 @@
-var elements;
-var windowHeight;
-
-
 function init() {
-  elements = document.querySelectorAll('.noanim');
-  windowHeight = window.innerHeight;
+  element = document.querySelector('.about-me');
+  
 }
 
 function checkPosition() {
-  for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-    var positionFromTop = elements[i].getBoundingClientRect().top;
-//console.log(positionFromTop,windowHeight);
-    if (positionFromTop - windowHeight <= -600) {
-      element.classList.add('animme');
+    
+    if (window.scrollY >= 700 && window.scrollY < 1300) {
       element.classList.remove('noanim');
+      element.classList.add('animme');
     }
-        if (positionFromTop - windowHeight > 0) {/*newly added:Edit2*/
-      element.classList.add('noanim');
-      element.classList.remove('animme');
+    if (window.scrollY < 400 && element.classList.contains('animme') === true) {
+        element.classList.remove('animme');
+        element.classList.add('noanim');
+        
     }
-  }
+    if (window.scrollY < 400 && element.classList.contains('animme') === false) {
+        return null;
+        
+    }
+    if (window.scrollY >= 1300 && element.classList.contains('noanim') === true) {
+        return null
+        
+    }
+    if (window.scrollY >= 1300 && element.classList.contains('noanim') === false) {
+        element.classList.remove('animme');
+        element.classList.add('noanim');
+        
+    }
+  
 }
 
 function socialInit() {
     menu = document.querySelector('.social-menu')
-    pageHeight = document.body.scrollHeight;
-    console.log(pageHeight)
+
 }
 
 
 function socialsMenu() {
-    if (window.scrollY >= 700 ) {
+    if (window.scrollY > 700 ) {
         menu.classList.remove('socialanimremove')
         menu.classList.add('socialanim')
     }
-    if (window.scrollY < 600 ) {
+    if (window.scrollY < 600 && menu.classList.contains('socialanim') === true) {
         menu.classList.remove('socialanim');
         menu.classList.add('socialanimremove');
+        
+    }
+    if (window.scrollY < 600 && menu.classList.contains('socialanim') === false) {
+        return null;
         
     }
    
@@ -50,5 +60,5 @@ window.addEventListener('scroll', checkPosition);
 window.addEventListener('resize', init);
 
 init();
-checkPosition();
+
 socialInit();
